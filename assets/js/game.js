44da54ee861ae26e84a45a8de1b15dae4a64519f
cmike212348 <cmike212348@gmail.com>
2020-08-07 13:09:21 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,7 +13,7 @@ console.log(playerName, playerAttack, playerHealth, playerMoney);
 
 //declare arrays
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = Math.floor(Math.random() *60);
 var enemyAttack = 12;
 console.log(enemyNames, enemyHealth, enemyAttack);
  
@@ -32,14 +32,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+   // generate random damage value based on player's attack power
+var damage = randomNumber(playerAttack - 3, playerAttack);
+
+enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -58,7 +60,9 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -85,7 +89,7 @@ for(var i = 0; i < enemyNames.length; i++) {
   
     var pickedEnemyName = enemyNames[i];
   
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
   
     fight(pickedEnemyName);
   //if we're not at the last enemy in the array
@@ -108,6 +112,12 @@ for(var i = 0; i < enemyNames.length; i++) {
  
 };
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+}
 // start the game when the page loads
 
 
